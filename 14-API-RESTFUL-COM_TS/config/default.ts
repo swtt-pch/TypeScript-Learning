de0d7c43d3a6bdfba8c57cf4530d2f2b287a36f9
swtt-pch <i.greygomes3@gmail.com>
2023-04-18 15:35:55 -0300

    environment variables

--- 14-API-RESTFUL-COM_TS/config/default.ts
+++ 14-API-RESTFUL-COM_TS/config/default.ts
@@ -1,4 +1,7 @@
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS,
 export default {
     port: 3000,
-    dbUri: "mongodb+srv://usrApiTs:T1OitPuoW6LWYHrk@cluster0.qc545ze.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    dbUri: 
+        `mongodb+srv://${dbUser}:${dbPassword}@cluster0.qc545ze.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 }
--- 14-API-RESTFUL-COM_TS/src/app.ts
+++ 14-API-RESTFUL-COM_TS/src/app.ts
@@ -1,3 +1,4 @@
+require("dotenv").config()
 import express from 'express'
 import config from 'config'
 import router from './router'

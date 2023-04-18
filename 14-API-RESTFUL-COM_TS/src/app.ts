af686f81bba6f9dfe2b355191faaae65bc762c77
swtt-pch <i.greygomes3@gmail.com>
2023-04-18 16:00:57 -0300

    morgan middleware

--- 14-API-RESTFUL-COM_TS/src/app.ts
+++ 14-API-RESTFUL-COM_TS/src/app.ts
@@ -3,14 +3,16 @@ import express from 'express'
 import config from 'config'
 import router from './router'
 import Logger from '../config/logger'
+import morganMiddleware from './middleware/morganMiddleware'
 import db from "../config/db"
 
 const app = express()
 app.use(express.json())
 
 const port = config.get<number>("port")
-
+app.use(morganMiddleware)
 app.use("/api/", router)
+
 
 app.listen(port, async () => {
     await db()

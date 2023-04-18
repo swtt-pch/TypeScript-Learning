77f8579260f8a4aed4881488016ecc76adbd123d
swtt-pch <i.greygomes3@gmail.com>
2023-04-18 16:56:01 -0300

    get all movies

--- 14-API-RESTFUL-COM_TS/src/controllers/movieControllers.ts
+++ 14-API-RESTFUL-COM_TS/src/controllers/movieControllers.ts
@@ -11,3 +11,36 @@ export async function createMovie(req:Request, res: Response) {
         Logger.error(`Erro no sistema: ${e.message}`)
     }
 }
+
+export async function findMovieById(req:Request, res: Response) {
+    try {
+        const id = req.params.id
+        const movie = await MovieModel.findById(id)
+
+        if(!movie) {
+            return res.status(404).json({error: "O filme não existe"})
+        }
+        return res.status(200).json(movie)
+    } catch (e:any) {
+        Logger.error(`Erro no sistema: ${e.message}`)
+    }
+}
+
+export async function getAllMovies(req:Request, res: Response) {
+    try {
+        const movies = await MovieModel.find()
+        if(movies.length === 0) return res.status(204).json({error: "Não possui filmes cadastrados"})
+        return res.status(200).json(movies)
+    } catch (e:any) {
+        Logger.error(`Erro no sistema: ${e.message}`)
+    }
+}
+
+
+export async function removeMovie(req:Request, res: Response) {
+    try {
+        //
+    } catch (e:any) {
+        Logger.error(`Erro no sistema: ${e.message}`)
+    }
+}
--- 14-API-RESTFUL-COM_TS/src/router.ts
+++ 14-API-RESTFUL-COM_TS/src/router.ts
@@ -1,5 +1,5 @@
 import {Router, Request, Response} from 'express'
-import { createMovie } from './controllers/movieControllers'
+import { createMovie, findMovieById, getAllMovies } from './controllers/movieControllers'
 import { validate } from './middleware/handleValidation'
 import { movieCreateValidation } from './middleware/movieValidation'
 
@@ -7,4 +7,7 @@ const router = Router()
 
 export default router.get("/test", (req: Request, res: Response) => {
     res.status(200).send("API Working")
-}).post("/movie", movieCreateValidation() ,validate ,createMovie)
+})
+.post("/movie", movieCreateValidation() ,validate ,createMovie)
+.get("/movie/:id", findMovieById)
+.get("/movie", getAllMovies)

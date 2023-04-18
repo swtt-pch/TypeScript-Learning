82eff16a14edce16e6aeb7e1cd48740240826807
swtt-pch <i.greygomes3@gmail.com>
2023-04-18 15:30:44 -0300

    connect to database

--- 14-API-RESTFUL-COM_TS/config/default.ts
+++ 14-API-RESTFUL-COM_TS/config/default.ts
@@ -1,3 +1,4 @@
 export default {
-    port: 3000
+    port: 3000,
+    dbUri: "mongodb+srv://usrApiTs:T1OitPuoW6LWYHrk@cluster0.qc545ze.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
--- 14-API-RESTFUL-COM_TS/src/app.ts
+++ 14-API-RESTFUL-COM_TS/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import config from 'config'
 import router from './router'
+import db from "../config/db"
 
 const app = express()
 app.use(express.json())
@@ -10,5 +11,6 @@ const port = config.get<number>("port")
 app.use("/api/", router)
 
 app.listen(port, async () => {
+    await db()
     console.log(`Aplicação está funcionando na porta: ${port}`)
 });
